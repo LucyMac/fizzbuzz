98e66f1c20dbfa31c77887dae4e8c36423283d0b
LucyMac <alessonadaykeepstheboredomaway@gmail.com>
2015-04-15 01:37:07 +1000

    added style

--- js/app.js
+++ js/app.js
@@ -7,25 +7,25 @@ $(document).ready(function() {
     					var node = document.createElement("P");         
 					    var textnode = document.createTextNode("FizzBuzz");
 					    node.appendChild(textnode);
-					    document.getElementById("container").appendChild(node);
+					    document.getElementById("numbers").appendChild(node);
 						}
 					else if (i % 3 === 0) {
 						var node = document.createElement("P");         
 					    var textnode = document.createTextNode("Fizz");
 					    node.appendChild(textnode);
-					    document.getElementById("container").appendChild(node);
+					    document.getElementById("numbers").appendChild(node);
 						}
 					else if (i % 5 === 0) {
 						var node = document.createElement("P");         
 					    var textnode = document.createTextNode("Buzz");
 					    node.appendChild(textnode);
-					    document.getElementById("container").appendChild(node);
+					    document.getElementById("numbers").appendChild(node);
 						}
 					else {
 						var node = document.createElement("P");         
-					    var textnode = i;
+					    var textnode = document.createTextNode(i);
 					    node.appendChild(textnode);
-					    document.getElementById("container").appendChild(node);
+					    document.getElementById("numbers").appendChild(node);
 						}
 				}
 		})
